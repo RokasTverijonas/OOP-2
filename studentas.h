d3d36e27408ea7a31fbdb7174e12124f265581dd
RokasTverijonas <tverijonasr@gmail.com>
2026-04-01 17:10:33 +0300

    Add student class, getters, setters

--- studentas.h
+++ studentas.h
@@ -4,13 +4,35 @@
 #include <string>
 #include <vector>
 
-struct studentas {
+
+class studentas {
+    
+    private:
     std::string vardas;
     std::string pavarde;
     std::vector<int> nd;
     int egzaminas;
     double galutinisVid;
     double galutinisMed;
+
+    public:
+    //getters
+    const std::string& getVardas() const { return vardas; }
+    const std::string& getPavarde() const { return pavarde; }
+    const std::vector<int>& getNd() const { return nd; }
+    int getEgzaminas() const { return egzaminas; }
+    double getGalutinisVid() const { return galutinisVid; }
+    double getGalutinisMed() const { return galutinisMed; }
+
+    //setters
+    void setVardas(const std::string& v) { vardas = v;}
+    void setPavarde(const std::string& p) { pavarde = p; }
+    void setNd(const std::vector<int>& n) { nd = n; }
+    void setEgzaminas(int e) { egzaminas = e; }
+    void setGalutinisVid(double galVid) { galutinisVid = galVid; }
+    void setGalutinisMed(double galMed) { galutinisMed = galMed; }
+
+
 };
 
 #endif
